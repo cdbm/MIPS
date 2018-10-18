faec36075810d3fa293168b4ed54eb7904e9642e
danbsts <dmrb2@cin.ufpe.br>
2018-10-18 09:38:48 -0300

    Fe que um dia acaba

---
+++ Mult.v
@@ -0,0 +1,6 @@
+module Mult(input wire [31:0] AFio, input wire [31:0] BFio, output wire [31:0] MultLoFio, output wire [31:0] MultHiFio);
+
+assign MultLoFio = AFio;
+assign MultHiFio = Bfio;
+
+endmodule
