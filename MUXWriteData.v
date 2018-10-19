f676c79faaa733cf52fbdb082879b9adec5a1d12
cdbm <cdbm@cin.ufpe.br>
2018-10-19 00:09:58 -0300

    final cpu

--- MUXWriteData.v
+++ MUXWriteData.v
@@ -1,46 +1,51 @@
-module MUXWriteData (input wire [2:0] MuxWriteData, input wire [31:0] AluOutFio , input wire [31:0] EPCFio , input wire [31:0] MDRFio,
+module MUXWriteData (input wire [3:0] MuxWriteData, input wire [31:0] AluOutFio , input wire [31:0] EPCFio , input wire [31:0] MDRFio,
 						input wire [31:0] Menor , input wire [31:0] RegDeslFio , input wire [31:0] SaidaPCFio, 
-		    				input wire [31:0] MuxHiLoFio, 
+		    				input wire [31:0] MuxHiLoFio, input wire [31:0] LuiFio,
 						output reg [31:0] WriteDataFio);	
 		
 	always begin
 		case(MuxWriteData)
-			3'b000:
+			4'b0000:
 			begin
 			WriteDataFio <= AluOutFio;
 			end
 			
-			3'b001:
+			4'b0001:
 			begin
 			WriteDataFio <= EPCFio;
 			end
 			
-			3'b010:
+			4'b0010:
 			begin
 			WriteDataFio <= MDRFio;
 			end
 			
-			3'b011:
+			4'b0011:
 			begin
 			WriteDataFio <= Menor;
 			end
 			
-			3'b100:
+			4'b0100:
 			begin
 			WriteDataFio <= RegDeslFio;
 			end
 			
-			3'b101:
+			4'b0101:
 			begin
 			WriteDataFio <= SaidaPCFio;
 			end
 			
-			3'b110:
+			4'b0110:
 			begin
 			WriteDataFio <= MuxHiLoFio;
 			end
 			
-			3'b111:
+			4'b0111:
+			begin
+			WriteDataFio <= LuiFio;
+			end
+			
+			4'b1000:
 			begin
 			WriteDataFio <= {24'b0,8'b11100011};
 			end
